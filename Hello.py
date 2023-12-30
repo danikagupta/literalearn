762af537eab94ea6a22463d6055c0001db3e4250
danikagupta <74515001+danikagupta@users.noreply.github.com>
2023-12-30 19:38:21 +0000

    Basic whisper integration

--- Hello.py
+++ Hello.py
@@ -15,62 +15,42 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import os
-import whisper
+from openai import OpenAI
 
-def transcribe_audio(file_path):
-    # Load the pre-trained Whisper model
-    model = whisper.load_model("base")  # You can choose other models like "small", "medium", "large"
+def transcribe_audio(file_path,language_iso):
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-    # Load the audio file
-    audio = whisper.load_audio(file_path)
-    audio = whisper.pad_or_trim(audio)
-
-    # (Optional) Print the detected language
-    # Mel spectrogram generation (optional to show the language detection)
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-    _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
-
-    # Transcribe the audio
-    options = whisper.DecodingOptions()
-    result = model.transcribe(mel, options)
-
-    # Return the transcription
-    return result["text"]
+    audio_file = open(file_path, "rb")
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file, 
+        language=language_iso,
+        response_format="text"
+    )
+ 
+    print(f"Transcript: {transcript}")
+    return transcript
 
 #
 # Main code
 #
+languages={"Hindi":"hi","English":"en","Malayalam":"ml"}
+sentences={"Hindi":"आप कैसे हैं?","English":"How are you?","Malayalam":"നിങ്ങൾ എങ്ങനെ ഉണ്ട്?"}
+
+
+language_select=st.selectbox("Select language",options=languages.keys())
+language_iso=languages[language_select]
+sentence=sentences[language_select]
+st.markdown(f"# Speak slowly and clearly (needs translation)")
+st.markdown(f"{sentence}")
 
 path_myrecording = os.path.join(os.getcwd(),"audiofiles","myrecording.wav")
 audio_bytes = audio_recorder(text="")
 if audio_bytes:
-    st.markdown("Just saw fresh audio bytes!")
     st.audio(audio_bytes, format="audio/wav")
     with open(path_myrecording, mode='bw') as f:
         f.write(audio_bytes)
         f.close()
-    transcription = transcribe_audio(path_myrecording)
-    st.markdown("Transcription:", transcription)
-
-aaa="""
-
-
-audio_bytes = audio_recorder(
-    text="",
-    recording_color="#e8b62c",
-    neutral_color="#6aa36f",
-    icon_name="user",
-    icon_size="6x",
-)
-
-
-
-from st_audiorec import st_audiorec
-
-wav_audio_data = st_audiorec()
-
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav') 
-st.write("# Welcome to Streamlit! 👋")
-"""
+    transcription = transcribe_audio(path_myrecording,language_iso)
+    st.markdown(f"Transcription: {transcription}")
+ 
