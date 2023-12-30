61ed703d67eebf864884d7aa95974ad34df8ad26
danikagupta <74515001+danikagupta@users.noreply.github.com>
2023-12-30 22:29:07 +0000

    Updated

--- Hello.py
+++ Hello.py
@@ -16,6 +16,9 @@ import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import os
 from openai import OpenAI
+import nltk
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.tokenize import word_tokenize
 
 
 def transcribe_audio(file_path,language_iso):
@@ -32,12 +35,37 @@ def transcribe_audio(file_path,language_iso):
     print(f"Transcript: {transcript}")
     return transcript
 
+
+
+def bleu(hypothesis, reference):
+  """Computes the BLEU score between a hypothesis and a reference.
+
+  Args:
+    hypothesis: A string containing the hypothesis text.
+    reference: A string containing the reference text.
+
+  Returns:
+    A float representing the BLEU score.
+  """
+  # Tokenize the hypothesis and reference strings.
+  hypothesis_tokens = word_tokenize(hypothesis)
+  reference_tokens = word_tokenize(reference)
+  print(f"Reference tokens: {reference_tokens}, hypothesis tokens: {hypothesis_tokens}, reference: {reference}, hypothesis: {hypothesis}")
+  st.markdown(f"Reference tokens: {reference_tokens}, hypothesis tokens: {hypothesis_tokens}, reference: {reference}, hypothesis: {hypothesis}")
+  # Calculate the BLEU score.
+  bleu_score = sentence_bleu([reference_tokens], hypothesis_tokens)
+
+  return bleu_score
+
 #
 # Main code
 #
 languages={"Hindi":"hi","English":"en","Malayalam":"ml"}
 sentences={"Hindi":"आप कैसे हैं?","English":"How are you?","Malayalam":"നിങ്ങൾ എങ്ങനെ ഉണ്ട്?"}
 
+nltk_data_path = os.path.join(os.getcwd(),"nltk_data")
+if nltk_data_path not in nltk.data.path:
+    nltk.data.path.append(nltk_data_path)
 
 language_select=st.selectbox("Select language",options=languages.keys())
 language_iso=languages[language_select]
@@ -54,4 +82,6 @@ if audio_bytes:
         f.close()
     transcription = transcribe_audio(path_myrecording,language_iso)
     st.markdown(f"Transcription: {transcription}")
+    sc=bleu(transcription,sentence)
+    st.markdown(f"BLEU score: {sc}")
  
